210cb038547a9a714746ec2f24cf81fe7358b182
LuckRoot <1547133608@qq.com>
2024-11-23 22:43:34 +0800

    在js当中变量你给什么数据，那么它就是什么类型，所以在查询数据库返回的时候，无需创建对应实体类

--- src/userModule/userModule.service.ts
+++ src/userModule/userModule.service.ts
@@ -8,10 +8,18 @@ export const getMsg = async () =>{
     // }
 
     // 操作数据库
-    const statement = `select * from user_notes`;
+    const statement = `
+        SELECT 
+        note.noteid, note.title, note.content, note.userno, u.id, u.uname, 
+        JSON_OBJECT('id', u.id, 'name', u.uname) AS userMsg
+        FROM user_notes note
+        JOIN system_user u
+        ON note.userno = u.id
+    `;
 
     const [MyData] = await connection.promise().query(statement);
 
+    console.log(MyData);
 
     return MyData;
 }
